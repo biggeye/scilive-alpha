1e1e291256cbefc4d5868dea9b8244ba6bc0ca32
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-11 08:03:45 -0800

    Add files via upload

---
+++ app/login/newOAuth/page.jsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { createClient } from '@/utils/supabase/client'; // Import your configured Supabase client
+
+const NewOauthLogin = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [userId, setUserId] = useState(null);
+  
+  const supabase = createClient();
+ 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Ensure the user is logged in
+    if (!userId) {
+      console.error('User is not authenticated');
+      return;
+    }
+
+    // Insert data into Supabase - adjust table and column names as per your schema
+    const { data, error } = await supabase
+      .from('profiles')
+      .insert([{ user_id: userId, first_name: firstName, last_name: lastName, email: email }]);
+
+    if (error) {
+      console.error('Error inserting data:', error);
+      return;
+    }
+
+    // Redirect to another page upon successful submission
+    window.location.href = '/dashboard';
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <code>{userId}</code>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="First Name"
+        required
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        placeholder="Last Name"
+        required
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default NewOauthLogin;
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,76 +1,23 @@
-'use client'
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { handleSignInWithPassword, handleOAuthLogin } from '@/lib/supabase-server';
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import GitHubLoginButton from "@/components/auth/GitHubLoginButton";
+import GoogleLoginButton from "@/components/auth/GoogleLoginButton";
 
-const LoginPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState('');
-    const router = useRouter();
+export default async function LoginPage() {
+  const cookieStore = cookies();
+  const supabase = createClient({ cookieStore });
 
-    const signInWithPassword = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setMessage('');
+  const { data: predictions } = await supabase.from("master_content").select("content-type");
 
-        try {
-            const result = await handleSignInWithPassword(email, password);
-            if (result.error) {
-                setMessage(result.error.message);
-            } else {
-                setMessage('Login successful!');
-                router.push('/');
-            }
-        } catch (error) {
-            setMessage(error.message);
-        } finally {
-            setLoading(false);
-        }
-    };
-    const signInWithGoogle = async () => {
-        setLoading(true);
-        setMessage('');
 
-        try {
-            const result = await handleOAuthLogin("google");
-            if (result.error) {
-                setMessage(result.error.message);
-            } else {
-                setMessage('Login successful!');
-                router.push('/');
-            }
-        } catch (error) {
-            setMessage(error.message);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return (
-        <div className="auth-form">
-            <form className="auth-form" onSubmit={signInWithPassword}>
-                <input 
-                    type="email" 
-                    value={email} 
-                    onChange={e => setEmail(e.target.value)} 
-                    placeholder="Email" 
-                />
-                <input 
-                    type="password" 
-                    value={password} 
-                    onChange={e => setPassword(e.target.value)} 
-                    placeholder="Password" 
-                />
-                <button type="submit" disabled={loading}>Sign In</button>
-            </form>
-            <button onClick={signInWithGoogle} disabled={loading}>
-                Sign In With Google
-            </button>
-            {message && <p>{message}</p>}
-        </div>
-    );
-};
-
-export default LoginPage;
+  return (
+    <>
+      <div className="auth-form"> 
+        <GoogleLoginButton />
+        <GitHubLoginButton />
+        <pre>{JSON.stringify(predictions, null, 2)}</pre>
+      </div>
+      
+    </>
+  );
+}

bb779194cbba27600eed011de6049de85651af35
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-13 20:34:47 -0800

    Update route.js

--- app/api/replicate/chat/route.js
+++ app/api/replicate/chat/route.js
@@ -1,7 +1,11 @@
+'use server'
+import { NextRequest, NextResponse } from 'next/server'
+
 const modelVersion =
   "04e422a9b85baed86a4f24981d7f9953e20c5fd82f6103b74ebc431588e1cec8";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextRequest) {
+
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {

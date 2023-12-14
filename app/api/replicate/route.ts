7a19b05132bea8a0494449d215b3c8b29493fe11
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-13 19:47:39 -0800

    Update route.ts

--- app/api/replicate/route.ts
+++ app/api/replicate/route.ts
@@ -3,7 +3,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers';
-&
+
 export default async function handler(req: NextRequest) {
     const cookieStore = cookies();
   const supabase = createClient(cookieStore)

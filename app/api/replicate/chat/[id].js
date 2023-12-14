01c37dbffff5aeb0134f1b841ccb8f2a4c5c0bf3
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-14 03:21:45 -0800

    Update [id].js

--- app/api/replicate/chat/[id].js
+++ app/api/replicate/chat/[id].js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function GET(req, res) {
   const response = await fetch(
     "https://api.replicate.com/v1/predictions/" + req.query.id,
     {

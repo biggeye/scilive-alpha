4983a29858b519ff7ee9d070d37d5e1a72d2d6a0
biggeye <justin_bonner@protonmail.com>
2023-12-14 23:29:16 -0800

    .

--- app/api/replicate/chat/route.js
+++ app/api/replicate/chat/route.js
@@ -1,7 +1,7 @@
 'use server'
 import { NextRequest, NextResponse } from 'next/server';
 
-export default async function POST(req: NextRequest) {
+export default async function POST(NextRequest) {
 
 const modelVersion =
   "04e422a9b85baed86a4f24981d7f9953e20c5fd82f6103b74ebc431588e1cec8";

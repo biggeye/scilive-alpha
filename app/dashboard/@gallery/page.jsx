2d88f4015ee0eb305f43fc2c3acf8ea327daf30a
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-15 04:08:57 -0800

    Update page.jsx

--- app/dashboard/@gallery/page.jsx
+++ app/dashboard/@gallery/page.jsx
@@ -1,5 +1,4 @@
-"use client";
-
+// Assuming "use client" is to be removed or replaced with a valid import
 import { createClient } from "@/utils/supabase/client";
 import Modal from "@/components/Modal";
 import React, { useState, useEffect } from 'react';
@@ -10,13 +9,15 @@ const Gallery = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const supabase = createClient();
 
- const openModal = (item) => {
+  const openModal = (item) => {
     setSelectedItem(item);
     setIsModalOpen(true);
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
   useEffect(() => {
     const fetchContent = async () => {
       const { data, error } = await supabase.from("master_content").select("*");
@@ -25,11 +26,12 @@ const Gallery = () => {
         console.error("Error fetching content:", error);
       } else {
         setContentItems(data);
-        console.log("contentItems: ", contentItems);
+        // Consider logging outside this function to see the updated state
       }
     };
     fetchContent();
   }, []);
+
   const handleDelete = async (contentId) => {
     const { error } = await supabase
       .from("master_content")
@@ -45,19 +47,21 @@ const Gallery = () => {
     }
   };
 
-
-  
   return (
     <div className="gallery">
+      <ul>
         {contentItems.map((item) => (
-            <img src={item.url} />
+          <li key={item.content_id}> {/* Assuming content_id is unique */}
+            <img src={item.url} alt={item.description} /> {/* Added alt for accessibility */}
+          </li>
+        ))}
+      </ul>
       <Modal
         isOpen={isModalOpen}
         onClose={closeModal}
         item={selectedItem}
         handleDelete={handleDelete}
       />
-      ))}
     </div>
   );
 };

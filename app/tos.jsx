8fe6c6c3eb42398dddf13105d84ba18a6cb8f340
biggEye <96358030+biggeye@users.noreply.github.com>
2023-12-21 11:05:20 -0800

    Update tos.jsx

---
+++ app/tos.jsx
@@ -0,0 +1,40 @@
+const TermsOfService = () => {
+  return (
+    <div>
+      <h1>Terms of Service for Scilive.Cloud</h1>
+      <h3>Free Account Usage</h3>
+      <p>Introduction to Free Account</p>
+      <p>Scilive.Cloud offers a free account option designed for individual users engaging in creative and non-commercial activities. By signing up for a free account, users agree to adhere to these specific terms in addition to the general Terms of Service of Scilive.Cloud.</p>
+      <h4>1. Eligibility for Free Account</h4>
+      <p>The free account is available for individual users only. Users must use the account for creative and non-commercial purposes.</p>
+      <h4>2. Usage Guidelines</h4>
+      <p><strong>Creative Use:</strong> The account should be used to support creative endeavors such as research, learning, artistic projects, and personal development.</p>
+      <p><strong>Non-Commercial Use:</strong> The account must not be used for any commercial purposes. This includes, but is not limited to, selling services or products, advertising or promoting a business, or other activities intended for profit.</p>
+      <h4>3. Restrictions on Free Account</h4>
+      <p><strong>Limited Access:</strong> Free accounts may have limited access to certain features or resources available on Scilive.Cloud.</p>
+      <p><strong>No Commercial Exploitation:</strong> Users must not use the free account for any form of commercial exploitation.</p>
+      <h4>4. Intellectual Property</h4>
+      <p>Any content created using the free account remains the intellectual property of the user, subject to Scilive.Cloud’s overall terms regarding user-generated content.</p>
+      <h4>5. Account Termination</h4>
+      <ul>
+        <li>Scilive.Cloud reserves the right to terminate or suspend free accounts that are found to be in violation of these specific terms or the general Terms of Service.</li>
+      </ul>
+      <h4>6. Changes to Free Account Terms</h4>
+      <ul>
+        <li>Scilive.Cloud may modify the terms for free account usage at any time. Continued use of the free account after such changes constitutes acceptance of the new terms.</li>
+      </ul>
+      <h4>7. Disclaimer of Warranties</h4>
+      <ul>
+        <li>The free account is provided “as is” without warranties of any kind, either express or implied.</li>
+      </ul>
+      <h4>8. Limitation of Liability</h4>
+      <ul>
+        <li>Scilive.Cloud will not be liable for any damages arising out of the use or inability to use the free account.</li>
+      </ul>
+      <h4>9. Acknowledgment and Acceptance of Terms</h4>
+      <ul>
+        <li>By using the free account, users acknowledge that they have read, understood, and agree to be bound by these terms.</li>
+      </ul>
+    </div>
+  );
+};ccount, users acknowledge that they have read, understood, and agree to be bound by these terms.

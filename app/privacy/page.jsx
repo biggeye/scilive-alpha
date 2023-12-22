7fc10beabe1e694481d73797eb3b80a0a15f6ec5
biggeye <justin_bonner@protonmail.com>
2023-12-21 16:18:30 -0800

    .

---
+++ app/privacy/page.jsx
@@ -0,0 +1,62 @@
+const PrivacyPolicy = () => {
+    return (
+      <div>
+        <h1>Privacy Policy for Scilive.Cloud</h1>
+        <h2>Introduction</h2>
+        <p>At Scilive.Cloud, we are committed to protecting the privacy and security of our users. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you visit our website.</p>
+  
+        <h2>Information Collection</h2>
+        <ol>
+          <li><strong>Personal Information:</strong> We collect personal information that you voluntarily provide to us when registering for the website, such as your name, email address, and any other contact or personal information you choose to provide.</li>
+          <li><strong>Avatar Uploads:</strong> Please note that any avatars uploaded by users are public and can be viewed by other users and visitors.</li>
+          <li><strong>Content Generated:</strong> Any content you generate on Scilive.Cloud is considered private and is not shared publicly unless you choose to do so.</li>
+        </ol>
+  
+        <h2>Use of Information</h2>
+        <p>The information we collect is used for the following purposes:</p>
+        <ul>
+          <li>To create and manage your account.</li>
+          <li>To personalize your user experience.</li>
+          <li>To improve our website and services.</li>
+          <li>To communicate with you, including responding to your inquiries.</li>
+        </ul>
+  
+        <h2>Sharing of Information</h2>
+        <p>We do not sell, trade, or otherwise transfer your personally identifiable information to outside parties except in the following cases:</p>
+        <ul>
+          <li>To comply with legal processes.</li>
+          <li>To protect the rights and safety of our company, our users, and others.</li>
+          <li>In the event of a merger, acquisition, or sale of all or a portion of our assets.</li>
+        </ul>
+  
+        <h2>Data Security</h2>
+        <p>We implement a variety of security measures to maintain the safety of your personal information:</p>
+        <ul>
+          <li>Secure servers and data encryption.</li>
+          <li>Regular malware scanning.</li>
+          <li>Restricted access to personal information.</li>
+        </ul>
+  
+        <h2>Cookies and Tracking Technologies</h2>
+        <p>Scilive.Cloud may use cookies and similar tracking technologies to track activity on our website and hold certain information. These are used to understand and save user preferences for future visits and compile aggregate data about site traffic and site interactions.</p>
+  
+        <h2>Third-Party Services</h2>
+        <p>Our website may contain links to third-party websites or services. We are not responsible for the privacy practices of these third parties and encourage you to review their privacy policies.</p>
+  
+        <h2>User Rights</h2>
+        <p>You have certain rights regarding your personal information, including:</p>
+        <ul>
+          <li>The right to access and receive a copy of your personal information.</li>
+          <li>The right to request correction or deletion of your personal information.</li>
+          <li>The right to object to or restrict processing of your personal information.</li>
+        </ul>
+  
+        <h2>Changes to this Privacy Policy</h2>
+        <p>We reserve the right to update or change our Privacy Policy at any time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+  
+        <h2>Contact Us</h2>
+        <p>If you have any questions about this Privacy Policy, please contact us at [insert contact information].</p>
+      </div>
+    );
+  };
+  

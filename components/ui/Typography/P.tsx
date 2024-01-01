86c7a32b415d420461509ebf21b0f70fc3472907
biggEye <96358030+biggeye@users.noreply.github.com>
2024-01-01 02:31:21 -0800

    Delete components/ui directory

--- components/ui/Typography/P.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from '@/utils/cn';
-import { ComponentProps } from 'react';
-
-export function P({ className, ...rest }: ComponentProps<'p'>) {
-  const classNames = cn('leading-7', '&:not(:first-child):mt-6', className);
-  return <p className={classNames} {...rest}></p>;
-}
--- components/ui/social/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SocialPage = () => {
-
-    return(
-        <p>Coming Soon...</p>
-)}
-
-export default SocialPage;

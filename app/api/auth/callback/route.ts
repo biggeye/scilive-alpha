c740e88594f03f9c8e45f25c311900c132f5a37c
biggeye <justin_bonner@protonmail.com>
2024-01-05 00:01:17 -0800

    .

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,19 +1,28 @@
-import { createClient } from '@/utils/supabase/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server'
-import type { Database } from '@/types_db'
+export async function POST(request: Request) {
+  const cookieStore = cookies()
 
-export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
 
-  if (code) {
-    const supabase = createClient()
-    await supabase.auth.exchangeCodeForSession(code)
-  }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(`${process.env.DEFAULT_URL}dashboard`)
+  return Response.redirect(`${process.env.DEFAULT_URL}/dashboard`)
 }
--- utils/supabase/route.ts
+++ utils/supabase/route.ts
@@ -2,28 +2,28 @@
 
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import { SupabaseClient } from '@supabase/supabase-js';
 
-export function createClient(request: Request): SupabaseClient {
+export function createClient(request: Request) {
     const cookieStore = cookies()
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options })
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.set({ name, value: '', ...options })
+          },
         },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options })
-        },
-      },
-    }
-  )
+      }
+    )
+  
 
   return supabase;
 }

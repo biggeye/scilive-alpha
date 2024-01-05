1a532114edd0f6fcbeb3420f204d6deae41503b0
biggeye <justin_bonner@protonmail.com>
2024-01-05 14:29:56 -0800

    .

--- app/api/replicate/webhook/route.js
+++
@@ -1,9 +0,0 @@
-import uploadPrediction from "@/lib/replicate/uploadPrediction";
-
-export async function post(req, res) {
-    console.log("🪝 incoming webhook!", req.body.id);
-    const { prediction } = req.body;
-    console.log(prediction);
-    await uploadPrediction(prediction);
-    res.end();
-}

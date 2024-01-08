b5e33fd937d7be6fe0a40f5bd2642416fccc9268
biggeye <justin_bonner@protonmail.com>
2024-01-08 11:42:21 -0800

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import './globals.css';
 import { fonts } from './fonts';
 import { ClientLayout } from './ClientLayout';
-
+import Head from 'next/head';
 
 export const metadata = {
   title: 'SciLive',
@@ -17,13 +17,16 @@ export default async function RootLayout({
   return (
 
     <html lang='en' className={fonts.roboto_mono.variable}>
+<Head>
       <link rel="manifest" href="http://localhost:3000/manifest.json" />
+      <script src="https://tikapi.io/assets/js/popup.js" />
       <meta name="theme-color" content="#f0f0f0"/>
+      </Head>
       <body>
         <ClientLayout>
           {children}
         </ClientLayout>
-        <script src="https://tikapi.io/assets/js/popup.js"></script>
+       
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,42 @@
-'use client';
-import { useRouter } from "next/navigation";
-import { Card, CardFooter } from "@chakra-ui/react";
-import { useUserContext } from "@/lib/UserProvider";
+'use client'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Card, CardFooter } from '@chakra-ui/react';
+import { useUserContext } from '@/lib/UserProvider';
+
 const Home = () => {
   const router = useRouter();
   const { userProfile } = useUserContext();
-  const id = userProfile.id;
-  if (!id) {
-    router.push('/login');
+
+  useEffect(() => {
+    // Redirect to /login if the user is not logged in
+    if (!userProfile?.id) {
+      router.push('/login');
+    }
+  }, [userProfile, router]);
+
+  // Render placeholder or loader while userProfile is being determined
+  if (!userProfile) {
+    return <div>Loading...</div>; // Or any other placeholder
   }
+
   return (
     <Card>
-    <CardFooter className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-      <p>
-        Powered by{' '}
-        <a href="https://www.scifiction.com" target="_blank" className="font-bold hover:underline" rel="noreferrer">
-          <img src="/scifiction.png" alt="SciFiction" width="70px" />
-        </a>
-      </p>
-    </CardFooter>
+      <CardFooter className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{' '}
+          <a
+            href="https://www.scifiction.com"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            <img src="/scifiction.png" alt="SciFiction" width="70px" />
+          </a>
+        </p>
+      </CardFooter>
     </Card>
-  )
-}
+  );
+};
 
 export default Home;

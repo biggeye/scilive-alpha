aead25751541085494e43e253514282ae40fe639
biggEye <96358030+biggeye@users.noreply.github.com>
2024-01-08 19:19:30 -0800

    Update route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
 
   if (code) {
     const cookieStore = cookies()
-    const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
+    const supabase = createClient(cookieStore)
     await supabase.auth.exchangeCodeForSession(code)
   }
 

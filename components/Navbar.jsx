403b681f313381e4944dc3d25735141214d471d7
biggeye <justin_bonner@protonmail.com>
2024-01-08 22:44:32 -0800

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -76,7 +76,7 @@ const UserMenu = ({ userImageUrl }) => {
         {userId ? (
           <MenuItem onClick={signOut}>Sign-Out</MenuItem>
         ) : (
-          <MenuItem as={Link} href="/">
+          <MenuItem as={Link} href="/login">
             Login
           </MenuItem>
         )}

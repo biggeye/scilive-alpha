606a1cff75eb6ed991ff71ecdaee61b18170b343
biggEye <96358030+biggeye@users.noreply.github.com>
2024-01-08 19:14:11 -0800

    Update middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { createClient } from '@/utils/supabase/middleware'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+
+  // Create a Supabase client configured to use cookies
+  const supabase = createClient({ req, res })
+
+  // Refresh session if expired - required for Server Components
+  await supabase.auth.getSession()
+
+  return res
+}
+
+// Ensure the middleware is only called for relevant paths.
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+}

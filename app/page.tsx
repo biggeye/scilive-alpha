7d92bf5384a12f38dbeb5f59c24a9fba4ee4042d
biggeye <justin_bonner@protonmail.com>
2024-01-09 07:25:33 -0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+'use client'
 import { useUserContext } from "@/lib/UserProvider";
-import { Text } from "@chakra-ui/react";
+import { Text, Center } from "@chakra-ui/react";
 import { Link
  } from "@chakra-ui/react";
 function Home() {
   const { userProfile } = useUserContext();
   return(
+    <Center>
      userProfile ? (
       <Text>Hello {userProfile.id}</Text>
      )  : (
       <Text>Click <Link href="/login">here</Link> to login.</Text>
      )
-     
+     </Center>
   )
 }
 

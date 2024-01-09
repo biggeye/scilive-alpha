d2e7252828020d5bcf82455337fe6b04739e3f92
biggEye <96358030+biggeye@users.noreply.github.com>
2024-01-08 19:30:07 -0800

    Update route.js

--- app/api/auth/confirm/route.js
+++ app/api/auth/confirm/route.js
@@ -1,30 +1,30 @@
-const { createServerClient } = require('@supabase/ssr')
-const { cookies } = require('next/headers')
-const { NextRequest, NextResponse } = require('next/server')
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
 
-
-export async function GET(request) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type')
-  const next = searchParams.get('next') || '/'
+  const type = searchParams.get('type') as EmailOtpType | null
+  const next = searchParams.get('next') ?? '/'
   const redirectTo = request.nextUrl.clone()
   redirectTo.pathname = next
 
   if (token_hash && type) {
     const cookieStore = cookies()
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          get(name) {
-            return cookieStore.get(name) ? cookieStore.get(name).value : null
+          get(name: string) {
+            return cookieStore.get(name)?.value
           },
-          set(name, value, options) {
+          set(name: string, value: string, options: CookieOptions) {
             cookieStore.set({ name, value, ...options })
           },
-          remove(name, options) {
+          remove(name: string, options: CookieOptions) {
             cookieStore.delete({ name, ...options })
           },
         },

f1b9ae1681aa132d0644a1ca069c508d878b121d
biggeye <justin_bonner@protonmail.com>
2024-01-11 13:10:00 -0800

    update api routes

--- app/api/content/getModels/img2txt/route.ts
+++ app/api/content/getModels/img2txt/route.ts
@@ -1,27 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
+
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import { getImg2TxtModels } from "@/lib/supabase-server";
 
+export default async function GET(req: Request) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_URL}/...`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     const session = await supabase.auth.getSession();
 
-  if (!session) {
-    return res.status(401).json({
-      error: 'not_authenticated',
-      description: 'The user does not have an active session or is not authenticated',
-    })
-  }
-
     if(req.method === 'GET'){
       const modelsData = await getImg2TxtModels();
-      return res.status(200).json(modelsData);
+      return Response.json([ modelsData ])
    }
    else{
-      return res.status(405).json({error: 'Method not allowed'});
+    return Response.json({error: 'Method not allowed'});
    }
 }

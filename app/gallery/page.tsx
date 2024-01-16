5f67c27fdf071cbd310af2e9e2e1c7477fbbfba3
biggEye <96358030+biggeye@users.noreply.github.com>
2024-01-16 05:04:45 -0800

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -15,7 +15,7 @@ const Gallery = () => {
   const [contentItems, setContentItems] = useState<ContentItem[]>([]);
   const [isThumbnailView, setIsThumbnailView] = useState(false);
   const { supabase } = useUserContext();
-  
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,6 +41,10 @@ const Gallery = () => {
     setIsThumbnailView(!isThumbnailView);
   }
 
+const handleImageClick = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <Box>
       <FormControl display="flex" alignItems="center" marginBottom="1em">
@@ -54,10 +58,18 @@ const Gallery = () => {
         <Box display="flex" flexWrap="wrap" justifyContent="center" gap="20px">
           {contentItems.map((item) => (
             <Box bgColor="white" borderRadius="5px" padding="5px" key={item.content_id} maxWidth="100px">
-              <Image borderRadius="4px" src={item.url} alt={item.content_type} width="100" height="100" />
+              <Image borderRadius="4px" src={item.url} alt={item.content_type} width="100" height="100" onClick={handleImageClick}/>
               <Button bgColor="orange" onClick={() => handleDeleteClick(item.content_id)} size="xxs">del</Button>
             </Box>
           ))}
+<Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} size="4xl">
+          <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+          <ModalContent>
+            <ModalBody>
+              <Image src={contentItems[currentIndex]?.url} alt={contentItems[currentIndex]?.content_type} />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
         </Box>
       ) : (
         <GalleryThumbnail />

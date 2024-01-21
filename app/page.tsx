df6c4fa9c3f40a7af8a0778562883af7f63bc3ed
biggEye <96358030+biggeye@users.noreply.github.com>
2024-01-20 16:03:38 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ function Home() {
 
   return (
     <Grid
-      templateAreas={`"hero hero"
-                      "feature additional"
-                      "footer footer"`}
+      templateAreas={`"hero hero hero"
+                      "feature feature additional"
+                      "footer footer footer"`}
       gridTemplateRows={'20vh 1fr 1fr'}
-      gridTemplateColumns={'1fc 70vw'}
+      gridTemplateColumns={'1fc 50vw auto'}
       h='100%'
       w='100vw'
       gap='1'
@@ -27,11 +27,11 @@ function Home() {
     <Button mt={4} colorScheme="teal" size="lg">Discover More</Button>
   </Box>
       </GridItem>
-
+<Box w="100%" bgColor="silver" borderColor="seasalt" borderWidth=".5px">
       <GridItem area={'feature'} as="section" className="dashboard-feature">
         <Text><Link href="/dashboard">sciGenerate</Link></Text>
       </GridItem>
-
+</Box>
       <GridItem area={'additional'} as="section" className="additional-info">
         <Text>About SciLive</Text>
         <Text as="p">Discover how SciLive is revolutionizing content creation...</Text>
@@ -39,7 +39,7 @@ function Home() {
 
       <GridItem area={'footer'} as="section" className="cta">
 <Box w="90vw" bgColor="onyx" borderColor="orange" borderWidth=".5px">
-        <Link href="/loginp"><a>Join SciLive Today</a></Link>
+        <Link href="/login"><a>Join SciLive Today</a></Link>
 </Box>
       </GridItem>
 

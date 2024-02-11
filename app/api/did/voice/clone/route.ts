31c043a9fd30eaa5c441952809ebaf420e75b1d4
biggeye <justin_bonner@protonmail.com>
2024-02-10 18:15:49 -0800

    .

--- app/api/did/voice/clone/route.ts
+++ app/api/did/voice/clone/route.ts
@@ -1,4 +1,4 @@
-// Import necessary utilities and functions
+import { NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/route';
 import fetch from 'node-fetch'; // Ensure you're using a fetch polyfill compatible with your environment
 import FormData from 'form-data'; // 'form-data' library for server-side form handling
@@ -8,46 +8,40 @@ export async function POST(req: any) {
   const session = await supabase.auth.getSession();
 
   if (!session) {
-    return {
+    return new NextResponse(JSON.stringify({
       error: 'not_authenticated',
       description: 'The user does not have an active session or is not authenticated',
-      status: 401
-    };
+    }), { status: 401 });
   }
 
-  // Assuming you've parsed the incoming FormData correctly into 'req.body'
-  // This might involve using middleware specific to your server framework,
-  // for example, 'multer' for Express.js, or a similar library for other frameworks.
 
-  // Extract the form fields and file from the parsed incoming request
   const name = req.body.name; // The string variable
   const file = req.body.file; // The file variable
+
 
   try {
     const dIdFormData = new FormData();
     dIdFormData.append('name', name);
-    // Assuming 'file' is a Blob or File-like object with 'stream', 'filename', and 'mimetype' properties
-    // This might need adjustment based on how your environment represents uploaded files
     dIdFormData.append('file', file.stream(), file.filename);
 
     const dIdResponse = await fetch(`https://api.d-id.com/talks/streams`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${process.env.NEXT_PUBLIC_DID_BEARER_TOKEN}`,
-        ...dIdFormData.getHeaders(), // Spread operator to include content-type boundary
+        ...dIdFormData.getHeaders(),
       },
       body: dIdFormData,
     });
 
     if (!dIdResponse.ok) {
       const error = await dIdResponse.json();
-      return { error: error, status: dIdResponse.status };
+      return new NextResponse(JSON.stringify(error), { status: dIdResponse.status });
     }
 
     const dIdData = await dIdResponse.json();
-    return { data: dIdData, status: 200 };
+    return new NextResponse(JSON.stringify(dIdData), { status: 200 });
   } catch (error) {
     console.error('Error making API call to D-ID:', error);
-    return { error: 'Internal server error', status: 500 };
+    return new NextResponse(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
   }
 }

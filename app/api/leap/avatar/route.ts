ef9dc9ab1228fd4692203ce1baa4b15b40555fe7
biggeye <justin_bonner@protonmail.com>
2024-02-18 17:41:07 -0800

    .

--- app/api/leap/avatar/route.ts
+++ app/api/leap/avatar/route.ts
@@ -1,22 +1,11 @@
-import { createClient } from '@/utils/supabase/route'
-
 export async function POST(req: Request) {
-  const supabase = createClient(req);
-  const session = await supabase.auth.getSession();
-
-  if (!session) {
-    console.log("request not authenticated")
-    return new Response(JSON.stringify({
-      error: 'not_authenticated',
-      description: 'The user does not have an active session or is not authenticated',
-    }), { status: 401 })
-  }
 
    try {
     const bodyData = await req.json();
-    const { avatar_name, avatar_description, user_id } = bodyData;
-    console.log(bodyData);
-    console.log(process.env.NEXT_PUBLIC_LEAP_API_KEY);
+    const avatar_name = bodyData.avatar_name;
+    const avatar_description = bodyData.avatar_description;
+    const user_id = bodyData.user_id;
+    
     const response = await fetch("https://api.workflows.tryleap.ai/v1/runs", {
           method: 'POST',
           headers: {

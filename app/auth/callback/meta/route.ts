0df93075a326c1d84f96b042761fec3fa953b1b2
biggeye <justin_bonner@protonmail.com>
2024-02-23 07:03:48 -0800

    .

--- app/auth/callback/meta/route.ts
+++ app/auth/callback/meta/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
 // app/api/auth/callback/route.ts
 export const runtime = "experimental-edge"
-export default async function post(req: NextRequest) {
+export async function post(req: NextRequest) {
   const url = req.nextUrl.clone();
   const code = url.searchParams.get('code');
 

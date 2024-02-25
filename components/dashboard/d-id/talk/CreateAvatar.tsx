0e4ae823e25bf668f63995d0873745f760156c48
biggEye <96358030+biggeye@users.noreply.github.com>
2024-02-24 21:59:24 -0800

    Update CreateAvatar.tsx

--- components/dashboard/d-id/talk/CreateAvatar.tsx
+++ components/dashboard/d-id/talk/CreateAvatar.tsx
@@ -5,136 +5,20 @@ import { Heading, Card, Box, Button, FormControl, FormLabel, Textarea, useToast,
 import { avatarNameState, avatarDescriptionState, avatarUrlState } from '@/state/createTalk-atoms';
 import { useUserContext } from '@/lib/user/UserProvider';
 import { imageArrayState } from '@/state/createTalk-atoms';
-import { createClient } from '@/utils/supabase/client';
+import AvatarCreator from '@/components/AvatarCreator';
 
 interface CreateAvatarProps {
   onCompleted: () => void;
 }
 
 const CreateAvatar: React.FC<CreateAvatarProps> = ({ onCompleted }) => {
-  const supabase = createClient();
 
-  const imageArray = useRecoilValue(imageArrayState);
-  const avatarName = useRecoilValue(avatarNameState);
-  const [avatarDescription, setAvatarDescription] = useRecoilState(avatarDescriptionState);
-  const [avatarUrl, setAvatarUrl] = useRecoilState(avatarUrlState);
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
-  const [polling, setPolling] = useState(false); // A state to trigger polling
-  const [images, setImages] = useState<string[]>([]);
-  const [selectedImage, setSelectedImage] = useState<string>('');
   const toast = useToast();
-
   const { userProfile } = useUserContext();
   const userId = userProfile.id;
 
-  useEffect(() => {
-    const subscription = supabase
-      .channel('custom-insert-channel')
-      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'master_content' }, (payload) => {
-        console.log('Change received!', payload);
-        if (payload.new && payload.new.url) {
-          setImages(currentImages => [...currentImages, payload.new.url]);
-        }
-      })
-      .subscribe();
-    return () => {
-      supabase.removeChannel(subscription);
-    };
-  }, []);
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsLoading(true);
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_URL}/api/leap/avatar`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          avatar_name: avatarName,
-          avatar_description: avatarDescription,
-          user_id: userId,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-
-      const responseData = await response.json();
-      if (responseData.status === "running") {
-        toast({
-          title: 'Processing',
-          description: 'Your avatar creation has started. You will be notified upon completion.',
-          status: 'info',
-          duration: 9000,
-          isClosable: true,
-        });
-
-      } else {
-        console.log(responseData);
-      }
-    } catch (error) {
-      console.error('Error running the workflow', error);
-      toast({
-        title: 'Error',
-        description: `Error running the workflow: ${error}`,
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  };
-
-
   return (
-    <Card p={5} boxShadow="xl" rounded="md" bg="white" width={{ base: '80vw', md: '60vw' }}>
-      <VStack spacing={4}>
-        <form onSubmit={handleSubmit}>
-
-          <Heading
-            mb={9}>Avatar Creator</Heading>
-          <FormControl isRequired>
-            <FormLabel>Avatar Description</FormLabel>
-            <Textarea value={avatarDescription || ''} onChange={(e) => setAvatarDescription(e.target.value)} placeholder="Describe your avatar" />
-
-            <Button type="submit" size="lg" width="full">
-              Submit
-            </Button>
-          </FormControl>
-          {isLoading ? (
-            <CircularProgress isIndeterminate color="blue.300" />
-          ) : (
-            <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(2, 1fr)" gap={4}>
-              {images.map((imgUrl, index) => (
-                <GridItem key={index} colSpan={1}>
-                  <Image src={imgUrl} alt={`Avatar Image ${index + 1}`} />
-                  <Checkbox isChecked={selectedImageIndex === index} onChange={() => setSelectedImageIndex(index)} />
-                </GridItem>
-              ))}
-            </Grid>
-          )}
-          <FormControl>
-            <Button
-
-              isDisabled={selectedImageIndex === null}
-              onClick={() => {
-                if (selectedImageIndex !== null) {
-                  setAvatarUrl(images[selectedImageIndex]);
-                  onCompleted();
-                }
-              }}
-            >
-              Select Avatar
-            </Button>
-          </FormControl>
-        </form>
-      </VStack>
-
-    </Card>
-
+   <AvatarCreator complete={onCompleted}/>
   );
 };
 
